0629c770011d06777765716397a9f49ddcba233e
Will914 <57054984+will803@users.noreply.github.com>
2022-02-05 12:18:40 +1100

    changed the way the link bar works
    for my own convenience ofc

---
+++ links.js
@@ -0,0 +1,1 @@
+document.getElementById("menulinks").innerHTML = '<a href="code.html" class="links"> Random Code Stuff </a> - <a href="aboutme.html" class="links"> About Me </a> - <a href="index.html" class="links"> Main Page </a>';
