47d18209ded8fdd923ab34e628b694241913c0c2
Will914 <57054984+Miastonished@users.noreply.github.com>
2023-11-26 18:55:16 +1100

    massive update yolo release

--- fts.js
+++ fts.js
@@ -7,12 +7,11 @@ function fts(){
     FTSheight = document.documentElement.getBoundingClientRect().height;
     FTSwidth = document.documentElement.getBoundingClientRect().width;
 
-
     silver.style.height = "44.734693877551020408163265306122px"; 
     silver.style.width = "32px"; 
     silver.style.position = "absolute";
-    silver.style.top = Math.floor(Math.random() * FTSheight+1);
-    silver.style.left = Math.floor(Math.random() * FTSwidth+1);
+    silver.style.top = Math.floor(Math.random() * FTSheight-31);
+    silver.style.left = Math.floor(Math.random() * FTSwidth-31);
     silver.style.backgroundImage = "url('https://miastonished.github.io/assets/silver.png')";
     silver.style.backgroundSize = "32px";
     silver.style.cursor = "pointer";
@@ -24,6 +23,10 @@ function fts(){
 function find(){
     FTSscore += 1;
     console.log('silver '+FTSscore);
-    silver.style.top = Math.floor(Math.random() * FTSheight+1);
-    silver.style.left = Math.floor(Math.random() * FTSwidth+1);
+    
+    FTSheight = document.documentElement.getBoundingClientRect().height;
+    FTSwidth = document.documentElement.getBoundingClientRect().width;
+    
+    silver.style.top = Math.floor(Math.random() * FTSheight-31);
+    silver.style.left = Math.floor(Math.random() * FTSwidth-31);
 }
