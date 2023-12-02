2432897db82e9adac8eb46f6a374a806118c174d
Will914 <57054984+Miastonished@users.noreply.github.com>
2023-12-02 13:00:21 +1100

    fts update

--- fts.js
+++ fts.js
@@ -4,8 +4,10 @@ let height = 0;
 let width = 0;
 
 function fts(numberofsilvers){
-    FTSheight = document.documentElement.getBoundingClientRect().height;
-    FTSwidth = document.documentElement.getBoundingClientRect().width;
+//    FTSheight = document.documentElement.getBoundingClientRect().height;
+//    FTSwidth = document.documentElement.getBoundingClientRect().width;
+    FTSheight = document.documentElement.offsetHeight;
+    FTSwidth = document.documentElement.offsetWidth;
     console.log(numberofsilvers);
     if(!Number.isInteger(numberofsilvers))
     {
@@ -32,8 +34,8 @@ function find(who){
     FTSscore += 1;
     console.log('silver '+FTSscore);
     
-    FTSheight = document.documentElement.getBoundingClientRect().height;
-    FTSwidth = document.documentElement.getBoundingClientRect().width;
+    FTSheight = document.documentElement.offsetHeight;
+    FTSwidth = document.documentElement.offsetWidth;
     
     who.style.top = Math.floor(Math.random() * FTSheight-31);
     who.style.left = Math.floor(Math.random() * FTSwidth-31);
