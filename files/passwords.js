950eeacceabc056053f1b1d4d5808afeedd16741
Will914 <57054984+Miastonished@users.noreply.github.com>
2024-03-11 20:35:08 +1100

    lock a01

---
+++ files/passwords.js
@@ -0,0 +1,88 @@
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//opening this file is not required for finishing the puzzle
+//nya
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let users = {
+    'passwords': {
+        admin: 'chugbingus6627',
+        silly915: 'password lol',
+        bob: undefined,
+    },
+    'comments': {
+        admin: 'default admin account',
+        silly915: ' ',
+        bob: 'Access Revoked',
+    },   
+};
