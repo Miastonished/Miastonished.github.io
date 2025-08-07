4a751909d4f966106a607a449bb245ce5612cdae
Will914 <57054984+Miastonished@users.noreply.github.com>
2025-08-07 21:33:23 +1000

    7 upd8 2025
    big fig wasp every wasp  you see Thats one of those !

---
+++ raspberries.js
@@ -0,0 +1,41 @@
+// i like haivng the first line of the file being a comment, then leaving a line between it and the first statement
+
+function init(){
+    
+
+    //setCookie("cookies", "true", 90);
+    //alert(getCookie("cookies"));
+    
+    
+}
+
+
+
+
+
+
+
+// Cookie functions below from w3 schools uwu https://www.w3schools.com/js/js_cookies.asp
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
